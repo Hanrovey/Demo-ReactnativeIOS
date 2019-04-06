3b5b7a2deee311be21ec35735ba2177612b6bc59
hanrovey <hanrovey@hanroveydeMacBook-Air.local>
2019-04-06 16:45:49 +0800

    rn和原生项目

---
+++ Demo-ReactnativeIOS/Swift-OC-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Swift-OC-Bridging-Header.h
+//  BlockChainStep
+//
+//  Created by Hanrovey on 2018/5/26.
+//  Copyright © 2018年 com. All rights reserved.
+//  swift调用OC的库，桥接文件
+
+#ifndef Swift_OC_Bridging_Header_h
+#define Swift_OC_Bridging_Header_h
+
+#import "RCTRootView.h"
+
+#endif /* Swift_OC_Bridging_Header_h */
---
+++ Demo-ReactnativeIOS/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Demo-ReactnativeIOS
+//
+//  Created by hanrovey on 2019/4/6.
+//  Copyright © 2019 hanrovey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func jump_Click(_ sender: Any) {
+        // 加载资源文件
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.ios.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        // 创建RN交互容器视图
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+}
+
